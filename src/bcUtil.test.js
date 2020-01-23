947fb187872d1b328a5366449c302d80367abe1f
poshjosh <posh.bc@gmail.com>
2020-01-23 22:29:43 +0100

    Refactor to reuse description for each test case

--- src/bcUtil.test.js
+++ src/bcUtil.test.js
@@ -1,13 +1,20 @@
 var assert = require('assert');
-var bcUtil = require('./bcUtil').default;
+var bcUtil = require('./bcUtil');
 
 describe('generateRandomUsername Test', function() {
-    it('should return value starting with prefix', function() {
-//        console.log("A this: " + this + ", prefix: " +prefix);
-        assert.equal(1 + 1, 2);
+    
+    var should_return_value_startswith_prefix = 'should return value starting with prefix';
+    it(should_return_value_startswith_prefix, function() {
+        var prefix = bcUtil.generateRandomUsername();
+        var result = bcUtil.generateRandomUsername(prefix);
+        assert.equal(result.indexOf(prefix), 0, should_return_value_startswith_prefix + ", but FAILED");
     });
-    it('should return different values full multiple calls', function() {
-//        console.log("B this: " + this + ", prefix: " +prefix);
-        assert.equal(3 * 3, 9);
+    
+    var should_return_different_values = 'should return different values for multiple calls';
+    it(should_return_different_values, function() {
+        var prefix = bcUtil.generateRandomUsername();
+        var result0 = bcUtil.generateRandomUsername(prefix);
+        var result1 = bcUtil.generateRandomUsername(prefix);
+        assert.notEqual(result0, result1, should_return_different_values + ", but FAILED");
     });
 });
